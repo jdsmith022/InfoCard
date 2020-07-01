2e6ce870a78422eb256afbe3ad9482ce23168a2d
Jessica Smith <jessicasmith@jessicadsmith.com>
2020-07-01 13:16:16 +0200

    adding gif

--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -1,55 +1,53 @@
 <template>
     <div id="InfoCard" class="info-card">
       <div class="info-icon">
-        <div>
-          <span class="info-content" v-on:mouseover="openCard">
-            <i class="fas fa-info-circle"></i>
-            <div v-show="active" class="display-card" v-on:mouseover="openCard">
-              <div class="card">
-                <div class="card-content">
-                  <div class="card-boxes">
-                    <div class="spacing">
-                      <div class="box info-box top-box">
-                        <div class="box-images">
-                          <div class="top-left-image">
-                            <img src="../assets/images/factory.png" />
-                          </div>
-                          <div class="top-right-image">
-                            <img src="../assets/images/truck.png" />
-                          </div>
+        <span class="info-content" v-on:mouseover="openCard">
+          <i class="fas fa-info-circle" ></i>
+          <div v-show="active" class="display-card" v-on:mouseover="openCard">
+            <div class="card">
+              <div class="card-content">
+                <div class="card-boxes">
+                  <div class="spacing">
+                    <div class="box info-box top-box">
+                      <div class="box-images">
+                        <div class="top-left-image">
+                          <img src="../assets/images/factory.png" />
                         </div>
-                        <p class="top-text">
-                          All greenhosue emissions from the
-                          <i><strong>manufacturing</strong></i> and <i><strong>shipping</strong></i>
-                          of your purchase is calculated.
+                        <div class="top-right-image">
+                          <img src="../assets/images/truck.png" />
+                        </div>
+                      </div>
+                      <p class="top-text">
+                        All greenhosue emissions from the
+                        <i><strong>manufacturing</strong></i> and <i><strong>shipping</strong></i>
+                        of your purchase is calculated.
+                      </p>
+                    </div>
+                  </div>
+                  <div class="card-boxes">
+                    <div class="box info-box">
+                      <div class="box-images">
+                        <div class="bottom-left-image">
+                          <img src="../assets/images/renewable.png" />
+                        </div>
+                      </div>
+                      <div class="box-text">
+                        <p>
+                          CO₂ok prevents the same amount
+                          of greenhosue emissions, making
+                          your purchase carbon neutral!
                         </p>
                       </div>
                     </div>
-                    <div class="card-boxes">
-                      <div class="box info-box">
-                        <div class="box-images">
-                          <div class="bottom-left-image">
-                            <img src="../assets/images/renewable.png" />
-                          </div>
-                        </div>
-                        <div class="box-text">
-                          <p>
-                            CO₂ok prevents the same amount
-                            of greenhosue emissions, making
-                            your purchase carbon neutral!
-                          </p>
-                        </div>
-                      </div>
-                    </div>
                   </div>
-                  <div class="bottom-text">
-                    <p>Learn how CO₂ offsetting <a @click="openTab()">works.</a></p>
-                  </div>
+                </div>
+                <div class="bottom-text">
+                  <p>Learn how CO₂ offsetting <a @click="openTab()">works.</a></p>
                 </div>
               </div>
             </div>
-          </span>
-        </div>
+          </div>
+        </span>
       </div>
     </div>
   </template>
