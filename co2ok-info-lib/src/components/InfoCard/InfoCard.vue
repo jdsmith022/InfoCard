fb8089b2cfcff509193da3c5b2d517f27342b25c
Jessica Smith <jessicasmith@jessicadsmith.com>
2020-07-01 10:49:43 +0200

    updating file

---
+++ co2ok-info-lib/src/components/InfoCard/InfoCard.vue
@@ -0,0 +1,83 @@
+<template>
+    <div id="InfoCard" class="info-card">
+      <div class="info-icon">
+        <div v-on:mouseover="openCard">
+          <span class="info-content" v-on:mouseover="openCard">
+            <i class="fas fa-info-circle"></i>
+            <div v-show="active" class="display-card">
+              <div class="card">
+                <div class="card-image">
+                </div>
+                <div class="card-content">
+                  <div class="card-boxes">
+                    <div class="spacing">
+                      <div class="box info-box">
+                        <div class="box-images">
+                          <div class="top-left-image">
+                            <img src="../assets/images/factory.png" />
+                          </div>
+                          <div class="top-right-image">
+                            <img src="../assets/images/truck.png" />
+                          </div>
+                        </div>
+                        <p>
+                          All greenhosue emissions from the
+                          <i><strong>manufacturing</strong></i> and <i><strong>shipping</strong></i>
+                          of your purchase is calculated.
+                        </p>
+                      </div>
+                    </div>
+                    <div class="card-boxes">
+                      <div class="box info-box">
+                        <div class="box-images">
+                          <div class="bottom-left-image">
+                            <img src="../assets/images/renewable.png" />
+                          </div>
+                        </div>
+                        <div class="box-text">
+                          <p>
+                            CO₂ok prevents the same amount
+                            of greenhosue emissions, making
+                            your purchase carbon neutral!
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="bottom-text">
+                    <p>Learn how CO₂ offsetting <a @click="openTab()">works.</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </span>
+        </div>
+      </div>
+    </div>
+  </template>
+
+<script>
+import './info_card.scss'
+
+export default {
+  name: 'InfoCard',
+  data() {
+    return {
+      active: false
+    }
+  },
+  methods: {
+    openCard() {
+      this.active = !this.active;
+    },
+    openTab() {
+      window.open("https://www.co2ok.eco/co2-compensatie");
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import './assets/sass/index.scss'
 import "@fortawesome/fontawesome-free/css/all.css"
 import "bulma/css/bulma.css"
+import co2okInfoCard from '../co2ok-info-lib'
 
+Vue.use(co2okInfoCard)
 
 Vue.config.productionTip = false
 
